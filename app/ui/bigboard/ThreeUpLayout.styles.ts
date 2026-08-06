d7195a7dd51cff2ce824b7c46f4ed750047e7a68
John Hamm <johndhamm1@gmail.com>
2026-08-06 15:57:31 -0400

    add ThreeUpLayout component

---
+++ app/ui/bigboard/ThreeUpLayout.styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { HEIGHTS, SCREEN_WIDTHS } from '@/app/styles';
+
+export const Container = styled.div`
+  width: 100%;
+  padding-top: ${HEIGHTS.NAVBAR_DESKTOP};
+  padding-bottom: calc(${HEIGHTS.BOTTOM_TICKER} + 2rem);
+  display: flex;
+  justify-content: center;
+  @media screen and (${SCREEN_WIDTHS.TABLET}) {
+    padding-top: ${HEIGHTS.NAVBAR_TABLET};
+  }
+`;
+
+export const CenterContent = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  width: 100%;
+  max-width: 440px;
+`;
+
+export const LeftContent = styled(CenterContent)`
+  @media screen and (${SCREEN_WIDTHS.TABLET}) {
+    display: none;
+  }
+`;
+
+export const RightContent = styled(LeftContent)`
+  @media screen and (${SCREEN_WIDTHS.DESKTOP}) {
+    display: none;
+  }
+`;
---
+++ app/ui/bigboard/ThreeUpLayout.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React, { JSX } from 'react';
+import {
+  CenterContent,
+  Container,
+  LeftContent,
+  RightContent,
+} from './ThreeUpLayout.styles';
+
+interface Props {
+  left: JSX.Element;
+  center: JSX.Element;
+  right: JSX.Element;
+}
+
+const ThreeUpLayout: React.FC<Props> = ({ left, center, right }) => {
+  return (
+    <Container>
+      <LeftContent>{left}</LeftContent>
+      <CenterContent>{center}</CenterContent>
+      <RightContent>{right}</RightContent>
+    </Container>
+  );
+};
+
+export default ThreeUpLayout;
